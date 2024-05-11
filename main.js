f66e2fbaa9f4fb79a26c398d23b9baf6a6932fbd
BAMAIBA- Rock le Messie <159274312+BAMAIBA@users.noreply.github.com>
2024-05-11 16:59:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".multiplex-text", {
+    strings: ["Frontend Developer", "YouTube","Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
